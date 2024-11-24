00e40600710e88723de2ed4f38ef1ac9f3c0cfc3
#V0ID <github@v0id.me>
2024-11-24 17:16:00 +0300

    refactor: moving fastapi-users to function to dependencies

--- app/api/v1/auth/__init__.py
+++ app/api/v1/auth/__init__.py
@@ -30,42 +30,11 @@ Security Features:
 """
 
 from fastapi import APIRouter
-from fastapi_users import FastAPIUsers
-from fastapi_users.authentication import AuthenticationBackend, CookieTransport
 
-from app.models.sessions import get_database_strategy
-from app.models.users import get_user_db
+from app.dependencies.fast_api_users import auth_backend, fastapi_users
 from app.schemas.users import UserCreate, UserRead, UserUpdate
 
 auth_router = APIRouter(tags=["Authentication"])
-
-# Cookie transport configuration
-# This defines how the session ID is transmitted in HTTP requests via cookies.
-cookie_transport = CookieTransport(cookie_max_age=3600)
-
-# Authentication backend configuration
-# This section configures the authentication backend, specifying how
-# sessions are managed.
-auth_backend = AuthenticationBackend(
-    name="session",  # Identifier for the authentication backend
-    transport=cookie_transport,  # Defines how sessions are transmitted
-    get_strategy=get_database_strategy,  # Defines how sessions are handled
-)
-
-# FastAPI Users instance configuration
-# This ties together all components:
-# - Database adapter
-# - Authentication backend
-# - User models and schemas
-fastapi_users = FastAPIUsers(
-    get_user_db,
-    [auth_backend],
-)
-
-# Current user retrieval
-# These variables provide access to the current active user and superuser.
-current_active_user = fastapi_users.current_user(active=True)
-current_superuser = fastapi_users.current_user(active=True, superuser=True)
 
 # Route Registration
 # Configures all authentication and user management endpoints.
